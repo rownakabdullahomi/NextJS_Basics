b2fa6347fed02c9fcc6771a519079c358d82bf24
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-04 02:13:26 +0600

    navbar and footer added

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul className="flex justify-center gap-5 pt-5">
+          <li>Home</li>
+          <li>Services</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
