2b8086631b63c4e102f3203e7bd7b3ee277e1fd2
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-04 02:49:25 +0600

    linking and nevigating done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const AboutPage = () => {
-    return (
-        <div>
-            <p className='font-bold text-3xl'>AboutPage</p>
-        </div>
-    );
+  const router = useRouter();
+  const isLoggedIn = true;
+  const handleNavigation = () => {
+    if (isLoggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  };
+  return (
+    <div>
+      <p className="font-bold text-3xl">AboutPage</p>
+      <Link href={"/about/address"}>Address Page</Link><br/>
+      <button type="button" onClick={handleNavigation}>
+        Address Page Button
+      </button>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -5,9 +6,11 @@ const Navbar = () => {
     <div>
       <nav className="bg-slate-600">
         <ul className="flex justify-center items-center gap-5 pt-1">
-          <li>Home</li>
-          <li>Services</li>
-          <li>About</li>
+          <Link href={"/"}>
+            <li>Home</li>
+          </Link>
+          <Link href={"/services"}><li>Services</li></Link>
+          <Link href={"/about"}><li>About</li></Link>
         </ul>
       </nav>
     </div>
