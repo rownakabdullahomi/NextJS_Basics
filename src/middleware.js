2df0e8cd6394f24f3f84e44bc78b995922e4378f
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-08 13:26:24 +0600

    working with cookies

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,28 @@
 import { NextResponse } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
 
-const dummyUserData = {
-    role: "user",
-    email: "admin@admin.com"
+    const currentCookie = request.cookies.get("nextjs-token")
+    console.log(currentCookie.value);
+
+    const dummyUserData = {
+        role: "user",
+        email: "admin@admin.com"
+    }
+
+
+    let isServicesPage = request.nextUrl.pathname.startsWith("/services");
+    let isAdmin = dummyUserData.role === "admin"
+
+    if (isServicesPage && !isAdmin) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+    return NextResponse.next()
 }
 
 
-let isServicesPage =  request.nextUrl.pathname.startsWith("/services");
-let isAdmin = dummyUserData.role === "admin"
-
-if(isServicesPage && !isAdmin){
-    return NextResponse.redirect(new URL("/login", request.url))
-}
-  return NextResponse.next()
-}
+// See "Matching Paths" below to learn more
+// export const config = {
+//     matcher: '/about/:path*',
+//   }
